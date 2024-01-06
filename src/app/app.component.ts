1957b4ca45b55db7dc172945df6898ef810fb117
karthik066-kr <karthikraja066.kr@gmail.com>
2024-01-06 08:43:17 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  
+  result:string='';
+  resultName= 'result';
+  add(firstNumber:string,secondNumber:string)
+  {
+    this.resultName="Add Result"
+    this.result = (Number(firstNumber)+Number(secondNumber)).toString(); 
+    
+  }
+  sub(firstNumber:string,secondNumber:string)
+  {
+    this.resultName="Sub Result"
+    this.result = (Number(firstNumber)-Number(secondNumber)).toString(); 
+    
+  }
+  mul(firstNumber:string,secondNumber:string)
+  {
+    this.resultName="Mul Result"
+    this.result = (Number(firstNumber)*Number(secondNumber)).toString(); 
+    
+  }
+  div(firstNumber:string,secondNumber:string)
+  {
+    this.resultName="Div Result"
+    this.result = (Number(firstNumber)/Number(secondNumber)).toString(); 
+    
+  }
 }
